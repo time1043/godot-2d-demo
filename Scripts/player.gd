f1f7abc1829b020bba686bda1787fcb3362da345
oswin <1665434994@qq.com>
2026-07-10 16:27:18 +0800

    feat(action): add player's run animation via asdw

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 @export var move_speed: float = 50
+@export var animator: AnimatedSprite2D
 
 ## Called when the node enters the scene tree for the first time.
 #func _ready() -> void:
@@ -9,6 +10,16 @@ extends CharacterBody2D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	# Accpet user input via keyboard, and set velocity
 	velocity = Input.get_vector("left", "right", "up", "down") * move_speed
+	
+	# Play idle animation when velocity == 0
+	# Play run animation when velocity != 0
+	if velocity == Vector2.ZERO:
+		animator.play("idle")
+	else:
+		animator.play("run")
+	
+	# Move according to velocity
 	move_and_slide()
